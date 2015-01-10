c475251b2f54624bef4fffee822dbdb945b8b5d4
Haydn Newport <haydn@catalyst.net.nz>
2015-01-10 16:27:35 +1300

    Move fps to title bar, prepare for switch to state machine

--- booth.py
+++ booth.py
@@ -1,13 +1,26 @@
 import os
 import time
 
+## Grab the backported enums from python3.4
+from enum import Enum
+
 import piggyphoto
 import pygame
 from pygame.locals import *
 
 COUNTDOWN = 3
-PREVIEW = '/tmp/preview.jpg'
+PREVIEW = '/mnt/tmp/preview.jpg'
 MAX_FPS = 8
+CAPTION = "Python Photobooth"
+WAITING = 'waiting'
+BEGIN = 'begin'
+QUIT = 'quit'
+
+class BoothState(Enum):
+  waiting = 1
+  shooting = 2
+  quit = 3
+
 
 class BoothView(object):
 
@@ -15,7 +28,7 @@ class BoothView(object):
   def __init__(self, width=640, height=480, fps=10, fullscreen=False):
     """Initialize the bits"""
     pygame.init()
-    pygame.display.set_caption("Python Photobooth")
+    pygame.display.set_caption(CAPTION)
     self.width = width
     self.height = height
     self.screen = pygame.display.set_mode((self.width, self.height), pygame.DOUBLEBUF)
@@ -29,6 +42,8 @@ class BoothView(object):
     self.camera.leave_locked()
     self.camera.capture_preview(PREVIEW)
 
+    self.state = BoothState.waiting
+
   def run(self):
     running = True
     while running:
@@ -38,25 +53,37 @@ class BoothView(object):
         elif event.type == pygame.KEYDOWN:
           if event.key == pygame.K_ESCAPE:
             running = False
-      milliseconds = self.clock.tick(self.fps)
+      milliseconds = self.clock.tick()
       self.playtime += milliseconds / 1000.0
       self.update_image()
-      self.draw_text("FPS: {:6.3}{}PLAYTIME: {:6.3} SECONDS".format(self.clock.get_fps(), " "*5, self.playtime))
+      pygame.display.set_caption("{} FPS: {:6.3}".format(CAPTION, self.clock.get_fps()))
       pygame.display.flip()
-      self.screen.blit(self.background, (0,0))
+      #self.screen.blit(self.background, (0,0))
 
     pygame.quit()
+
+  def wait_state(self):
+    for event in pygame.event.get():
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ENTER:
+          self.state = BoothState.shooting
+    self.update_image()
+    self.draw_centered_text('PRESS THE BIG RED BUTTON TO START')
+
+
 
   def update_image(self):
     self.camera.capture_preview(PREVIEW)
     picture = pygame.image.load(PREVIEW)
-    self.background.blit(picture, (0,0))
+    picture = pygame.transform.scale(picture, (self.width, self.height))
+    self.screen.blit(picture, (0,0))
 
-  def draw_text(self, text):
+  def draw_centered_text(self, text):
     """Center text in window"""
     fw, fh = self.font.size(text)
-    surface = self.font.render(text, True, (0,255,0))
-    self.screen.blit(surface, ((self.width - fw) / 2, (self.height - fh) / 2))
+    textobj = self.font.render(text, True, (255,255,255))
+    textpos = textobj.get_rect(centerx=self.background.get_width()/2, centery=self.background.get_height()/2)
+    self.screen.blit(textobj, textpos)
 
 if __name__ == '__main__':
   BoothView().run()
@@ -66,5 +93,5 @@ def quit_pressed():
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
         return True
-  # Don't quit... someone might be being a dickhead
+  # Don't quit... someone might be messing around
   return False
